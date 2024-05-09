062d4d975d75b50351ce875184edd1b2aaf2831b
unkrautCode <159149017+unkrautCode@users.noreply.github.com>
2024-05-09 16:08:45 +0200

    setup multiLoader
    scriptMash to create a 3d model loader script

--- src/models.jsx
+++ src/models.jsx
@@ -1,6 +1,39 @@
 import { useGLTF } from '@react-three/drei';
+import { useLoader } from '@react-three/fiber'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
 
 export default function Model({ ...props }) {
-  const { scene } = useGLTF('/models/CompuRoom.gltf');
+  const { scene } = useGLTF('/models/b2_Room.gltf');
+  const colorMap = useLoader(TextureLoader, '/models/b_Room.jpg')
+  // Verwende die Textur in dem Modell
+  scene.traverse((child) => {
+    if (child.isMesh) {
+      child.material.map = colorMap
+    }
+  })
   return <primitive object={scene} {...props} />;
 }
+
+
+/*
+// Laden der Farbtextur
+const colorMap = useLoader(TextureLoader, 'PavingStones092_1K_Color.jpg')
+
+const [colorMap, displacementMap, normalMap, roughnessMap, aoMap] = useLoader(TextureLoader, [
+  'PavingStones092_1K_Color.jpg',
+  'PavingStones092_1K_Displacement.jpg',
+  'PavingStones092_1K_Normal.jpg',
+  'PavingStones092_1K_Roughness.jpg',
+  'PavingStones092_1K_AmbientOcclusion.jpg',
+])
+
+// Texturen auf das Mesh anwenden
+<meshStandardMaterial
+  map={colorMap}
+  displacementMap={displacementMap}
+  normalMap={normalMap}
+  roughnessMap={roughnessMap}
+  aoMap={aoMap}
+/>
+
+*/
