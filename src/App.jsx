f578a557aedfac06ea88906e6cc8eceed70e8ee8
unkrautCode <159149017+unkrautCode@users.noreply.github.com>
2024-05-11 15:02:21 +0200

    Switch Colors of Lightbulb
    emission didnt work

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { Environment, OrbitControls, Stage } from '@react-three/drei';
 import {Room, Couch, Couchfuesse, Kissen1, Kissen2, Legdesk, Viernullvier, BtnLicht, BtnVenti, Maus, BtnMaus, Projektionsflaeche, KeyZ, KeyW, KeyT, KeySpace, KeyR, KeyQ, KeyEsc, KeyEnter, KeyE, KeyCtrl, LEDMoni, Monitor, Gluehbirne} from './models';
 
+
 function App() {	
 	
 	const [color, setColor] = useState('white'); // Zustand für die Farbe
@@ -13,6 +14,7 @@ function App() {
 	console.log(color);
     setColor(color === 'white' ? 'red' : 'white');
   };
+	
 
   return (
 	<Canvas style={{ width: '100vw', height: '100vh' }} camera={{ position: [2000, 10, 2000] }}>
@@ -32,7 +34,7 @@ function App() {
 				
 				{/* onClick-Ereignis für BtnLicht-Schalter */}
 				<mesh onClick={handleClick}>
-				<BtnLicht />
+				<BtnLicht/>
 				</mesh>
 				
 				<BtnVenti />
@@ -52,7 +54,9 @@ function App() {
 				<LEDMoni />
 				<Monitor />
 				{/* passt Farbe der Gluehbirne entsprechend dem internen Zustand an */}
-             	<Gluehbirne color ={color}  />
+				<mesh>
+            	<Gluehbirne color={color} />
+         		</mesh>
 			</>
 			}
   		</Stage>
@@ -62,3 +66,89 @@ function App() {
 }
 
 export default App;
+
+
+/* Emssive Ansatz
+import React, { useRef, useEffect, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Environment, OrbitControls, Stage } from '@react-three/drei';
+import { Room, Couch, Couchfuesse, Kissen1, Kissen2, Legdesk, Viernullvier, BtnLicht, BtnVenti, Maus, BtnMaus, Projektionsflaeche, KeyZ, KeyW, KeyT, KeySpace, KeyR, KeyQ, KeyEsc, KeyEnter, KeyE, KeyCtrl, LEDMoni, Monitor, Gluehbirne } from './models';
+
+const GlowingMaterial = ({ color }) => {
+  const materialRef = useRef();
+
+  useFrame(({ clock }) => {
+    if (materialRef.current) {
+      const intensity = Math.sin(clock.getElapsedTime()) * 0.5 + 0.5;
+      materialRef.current.emissive.set(color);
+      materialRef.current.emissiveIntensity = intensity;
+    }
+  });
+
+  return (
+    <meshBasicMaterial
+      ref={materialRef}
+      color={color}
+      emissive={color}
+      emissiveIntensity={0}
+    />
+  );
+};
+
+const App = () => {
+  const [color, setColor] = useState('red');
+
+  const ChangeColorOnClick = () => {
+    const handleClick = () => {
+      setColor((prevColor) => (prevColor === 'red' ? 'white' : 'red'));
+    };
+
+    return (
+      <mesh onClick={handleClick}>
+        <BtnLicht />
+      </mesh>
+    );
+  };
+
+  return (
+    <Canvas style={{ width: '100vw', height: '100vh' }} camera={{ position: [2000, 10, 2000] }}>
+      <Environment preset='studio' />
+
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <ChangeColorOnClick />
+      <Stage>
+        <>
+          <Room />
+          <Couch />
+          <Couchfuesse />
+          <Kissen1 />
+          <Kissen2 />
+          <Legdesk />
+          <Viernullvier />
+          <BtnVenti />
+          <Maus />
+          <BtnMaus />
+          <Projektionsflaeche />
+          <KeyZ />
+          <KeyW />
+          <KeyT />
+          <KeySpace />
+          <KeyR />
+          <KeyQ />
+          <KeyEsc />
+          <KeyEnter />
+          <KeyE />
+          <KeyCtrl />
+          <LEDMoni />
+          <Monitor />
+          <Gluehbirne color={color} />
+        </>
+      </Stage>
+      <OrbitControls />
+    </Canvas>
+  );
+}
+
+export default App;
+*/
