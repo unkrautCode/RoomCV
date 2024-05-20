ed2393f4bbd3e41084ca02fc57d1e17470acd2d0
unkrautCode <159149017+unkrautCode@users.noreply.github.com>
2024-05-20 15:13:08 +0200

    Sort rnd 2
    Anwendungskomponente aufgeräumt, zwischen save

--- src/Anwendung.jsx
+++ src/Anwendung.jsx
@@ -1,12 +1,14 @@
+import React from 'react';
+import { Text, Html, ContactShadows, PresentationControls, Float, Enviroment, useGLTF } from '@react-three/drei';}
 import { Room, Couch, Couchfuesse, Kissen1, Kissen2, Legdesk, Viernullvier, BtnLicht, BtnVenti, Maus, BtnMaus, Projektionsflaeche, KeyZ, KeyW, KeyT, KeySpace, KeyR, KeyQ, KeyEsc, KeyEnter, KeyE, KeyCtrl, LEDMoni, Monitor, Gluehbirne} from './models';
+import { Color } from 'three';
+import './App.css';
 
-{/* //alles baked, also ohne Env
-      <Environment background files={['/models/courtyard.jpg']} />	
-      */
-    }
-
-    {/**
-    <ambientLight intensity={0.5} />
+export default function Anwendung()
+{
+  return(
+    <>
+      <ambientLight intensity={0.5} />
       <spotLight position={[10, 15, 10]} angle={0.3} />  
       <Stage>
         {<>
@@ -39,9 +41,15 @@ import { Room, Couch, Couchfuesse, Kissen1, Kissen2, Legdesk, Viernullvier, BtnL
             <mesh>
                    <Gluehbirne color={color} />
              </mesh>
-        </>
-        }
+        </>}
       </Stage>
-    <OrbitControls />
-
-     */}
+      <PresentationControls />
+      <ContactShadows
+        position-y={ - 2.5 }
+        opacity={ 0.4 }
+        scale={ 5 }
+        blur={ 2.4 }
+      />
+    </>
+  );
+}
